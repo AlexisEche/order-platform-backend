020392264031f4e62c6ab60ea5d23df12483a899
AlexisEche <alexis.eche58@gmail.com>
2021-11-24 16:22:55 -0500

    add use URLField to image recipes model

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -5,11 +5,11 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Recipes(TimeStampedModel):
-	name = models.CharField(verbose_name=_("name"), max_length=50, null=True)
-	description = models.TextField(verbose_name=_("description"),null=True)
-	image = models.TextField(verbose_name=_("image"), max_length=255, null=True)
+	name = models.CharField(verbose_name=_("name"), max_length=50, blank=True, default="")
+	description = models.TextField(verbose_name=_("description"),blank=True, default="")
+	image = models.URLField(verbose_name=_("image"), blank=True, default="")
 	price = models.PositiveIntegerField(verbose_name=_("price"), default=0)
-	category = models.CharField(verbose_name=_("category"), max_length=50, null=True)
+	category = models.CharField(verbose_name=_("category"), max_length=50, blank=True, default="")
 	amount = models.PositiveIntegerField(verbose_name=_("amount"), default=0)
 
 # Create your models here.
--- core/settings.py
+++ core/settings.py
@@ -24,15 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", default="django-insecure-8vpzm^kgjjonsv=*oh4chw5-ol+zk5)9p2rvzq!0+#+k8_9ti^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="").split(" ")
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-
-CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_HOSTS").split(" ")
+CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_HOSTS", default="https://localhost:8080").split(" ")
 
 # Application definition
 
