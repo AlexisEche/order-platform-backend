451ab3b8e52eaccd1f3c11a7f20d889e07c25a7e
AlexisEche <alexis.eche58@gmail.com>
2021-11-24 15:42:04 -0500

    add use TextField max_length to image recipes model

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -7,7 +7,7 @@ from django.utils.translation import gettext_lazy as _
 class Recipes(TimeStampedModel):
 	name = models.CharField(verbose_name=_("name"), max_length=50, null=True)
 	description = models.TextField(verbose_name=_("description"),null=True)
-	image = models.TextField( max_length=500,verbose_name=_("image"), null=True)
+	image = models.TextField(verbose_name=_("image"), max_length=255, null=True)
 	price = models.PositiveIntegerField(verbose_name=_("price"), default=0)
 	category = models.CharField(verbose_name=_("category"), max_length=50, null=True)
 	amount = models.PositiveIntegerField(verbose_name=_("amount"), default=0)
